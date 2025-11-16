92dbca8f9354fba51789fab5cae909f02be8a85f
migarcos <migark@hotmail.com>
2025-11-16 18:55:19 -0500

    memory CRUD success

--- controllers/memoryCtrl.js
+++ controllers/memoryCtrl.js
@@ -15,8 +15,8 @@ const getSingle = async (req, res) => {
     const itemId = new ObjectId( req.params.id );
     const result = await mongodb.getDB().db('hardware').collection('memory').find({_id: itemId});
         result.toArray().then( (device) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(device);
+        result.setHeader('Content-Type', 'application/json');
+        result.status(200).json(device);
     });
 }
 
@@ -52,9 +52,64 @@ const memoryCreate = async (req, res) => {
     };
 }
 
+const deleteMemory = async (req, res) => {
+    // #swagger.tags = ['Memories']
+    try {
+        const deviceId = new ObjectId(req.params.id);
+
+        const response = await mongodb.getDB().db('hardware').collection('memory').deleteOne({ _id: deviceId});
+
+        if (response.deleteCount > 0) {
+            res.status(200).json( { message: 'Memory ID record deleted SUCCESSFULLY'})
+        } else {
+            res.status(404).json({ error: 'Memory ID not found or already deleted'});
+        }
+    } catch (err) {
+        console.error('Error deleting Memory ID:', err);
+        res.status(500).json({ err: 'Internal server error' });
+    }
+}
+
+const updateMemory = async (req, res) => {
+    // #swagger.tags = ['Memories']
+    try {
+        const deviceId = new ObjectId(req.params.id);
+
+        const updateFields = {};
+
+        if (req.body.manufacturer) updateFields.manufacturer = req.body.manufacturer;
+        if (req.body.model) updateFields.model = req.body.model;
+        if (req.body.presentation_date) updateFields.presentation_date = req.body.presentation_date;
+        if (req.body.initial_price_usd) updateFields.initial_price_usd = req.body.initial_price_usd;
+
+        if (req.body.characteristics) {
+            updateFields.characteristics = {};
+            const c = req.body.characteristics;
+            if (c.capacity) updateFields.characteristics.capacity = c.capacity;
+            if (c.generation) updateFields.characteristics.generation = c.generation;
+            if (c.speed) updateFields.characteristics.speed = c.speed;
+            if (c.latency) updateFields.characteristics.latency = c.latency;
+            if (c.form_factor) updateFields.characteristics.form_factor = c.form_factor;
+            if (c.features) updateFields.characteristics.features = c.features;
+        }
+
+        const response = await mongodb.getDB().db('hardware').collection('memory').updateOne({ _id: deviceId }, { $set: updateFields });
+
+        if (response.modifiedCount > 0) {
+            res.status(200).json({ message: 'Memmory updated successfully' });
+        } else {
+            res.status(404).json({ error: 'Memory not found or no changes applied' });
+        }
+    } catch (error) {
+        console.error('Error updating Memory:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
+
 module.exports = {
     getAll,
     getSingle,
-    memoryCreate
-
+    memoryCreate,
+    deleteMemory,
+    updateMemory
 }
--- routes/memory.js
+++ routes/memory.js
@@ -8,4 +8,8 @@ router.get('/memory/:id', memCtrler.getSingle);
 
 router.post('/memory', memCtrler.memoryCreate);
 
+router.delete('/memory/:id', memCtrler.deleteMemory);
+
+router.put('/memory/:id', memCtrler.updateMemory);
+
 module.exports = router;
