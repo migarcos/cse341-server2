6c2baf6c8dcaff8746d62d02471e18d678f9bc6c
migarcos <migark@hotmail.com>
2025-11-16 00:00:11 -0500

    validation processor success

--- routes/processor.js
+++ routes/processor.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const router = express.Router();
-// const { validationResult } = require("express-validator");
+const { validationResult } = require("express-validator");
 const processorCtrler = require('../controllers/processorCtrl');
-// const  { validateProcessor } = require('../middleware/validator');
+const  { validateProcessor, validateUpdProcessor } = require('../middleware/validator');
 
 router.get('/processor', processorCtrler.getAll);
 
 router.get('/processor/:id', processorCtrler.getSingle);
 
-router.post('/processor', processorCtrler.createProcessor);
+router.post('/processor', validateProcessor, async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+    return processorCtrler.createProcessor(req, res);
+});
 
-router.put('/processor/:id', processorCtrler.updateProcessor);
+router.put('/processor/:id', validateUpdProcessor, async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty() ) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+    return processorCtrler.updateProcessor(req, res);
+});
 
 router.delete('/processor/:id', processorCtrler.deleteProcessor);
 
